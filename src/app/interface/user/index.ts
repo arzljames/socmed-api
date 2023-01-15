635fefa6182eb6a5b6df5d1f73f915713908f2d6
Arzl James <arzljames15@gmail.com>
2023-01-16 01:38:54 +0800

    [fauture] user registration

--- src/app/interface/user/index.ts
+++ src/app/interface/user/index.ts
@@ -1,10 +1,31 @@
 import { RegisterUserDTO } from 'src/app/dto/auth';
-import { User } from 'src/app/model/user/user.schema';
 
 export abstract class AbstractUserRepository {
-  abstract create(user: RegisterUserDTO): Promise<User | null>;
+  abstract create(user: RegisterUserDTO): Promise<CreatedUserResponse | null>;
 }
 
 export abstract class AbstractUserService {
-  abstract create(user: RegisterUserDTO): Promise<User | null>;
+  abstract create(user: RegisterUserDTO): Promise<CreatedUserResponse | null>;
+}
+
+export interface CreatedUserResponse {
+  _id: string;
+  email: string;
+  username: string;
+  is_verfied: boolean;
+  status: string;
+  created_at: Date;
+  updated_at: Date;
+  profile: CreatedProfileInterface;
+}
+
+export interface CreatedProfileInterface {
+  _id: string;
+  first_name: string;
+  last_name: string;
+  middle_name?: string;
+  initials: string;
+  profile_color: string;
+  created_at: Date;
+  updated_at: Date;
 }
--- src/app/repository/user/user.repository.ts
+++ src/app/repository/user/user.repository.ts
@@ -1,19 +1,48 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
-import { Schema as MongooseSchema } from 'mongoose';
 import { Model } from 'mongoose';
 import { RegisterUserDTO } from 'src/app/dto/auth';
-import { AbstractUserRepository } from 'src/app/interface/user';
+import {
+  AbstractUserRepository,
+  CreatedUserResponse,
+} from 'src/app/interface/user';
 import { User, UserDocument } from 'src/app/model/user/user.schema';
 import * as bcrypt from 'bcrypt';
+import * as _ from 'lodash';
+import { DEFAULT_PROFILE_COLORS } from 'src/app/const';
+import { Profile, ProfileDocument } from 'src/app/model/profile/profile.schema';
 
 @Injectable()
 export class UserRepository implements AbstractUserRepository {
-  constructor(@InjectModel(User.name) private userModel: Model<UserDocument>) {}
-  async create(user: RegisterUserDTO): Promise<User | null> {
-    const createdUser = await this.userModel.create(user);
+  constructor(
+    @InjectModel(User.name) private userModel: Model<UserDocument>,
+    @InjectModel(Profile.name) private profileModel: Model<ProfileDocument>,
+  ) {}
+  async create(user: RegisterUserDTO): Promise<CreatedUserResponse | null> {
+    const { email, username, password, first_name, last_name } = user as any;
+    const hashPassword = await this.hashPassword(password);
+    const profileColor = this.pickProfileColor();
 
-    return createdUser;
+    const createUser = await this.userModel.create({
+      email,
+      username,
+      password: hashPassword,
+    });
+    const createProfile = await this.profileModel.create({
+      first_name: this.capitalizeWord(first_name),
+      last_name: this.capitalizeWord(last_name),
+      initials: this.getInitials(first_name, last_name),
+      profile_color: profileColor,
+    });
+
+    createUser.profile = createProfile._id;
+    createUser.save();
+    createProfile.save();
+
+    return await this.userModel
+      .findById({ _id: createUser._id }, { password: 0 })
+      .populate('profile')
+      .lean();
   }
 
   private async hashPassword(password: string): Promise<string | null> {
@@ -33,4 +62,37 @@ export class UserRepository implements AbstractUserRepository {
       return compare;
     }
   }
+
+  private pickProfileColor(): string {
+    const randomNumber = _.random(0, 5);
+    const randomColor = _.filter(
+      DEFAULT_PROFILE_COLORS,
+      (_item: Array<string>, index: number) => {
+        return index === randomNumber;
+      },
+    );
+
+    return randomColor[0];
+  }
+
+  private capitalizeWord = (word: string): string => {
+    if (word) {
+      const separatedWord = word.split(' ');
+
+      for (let i = 0; i < separatedWord.length; i++) {
+        separatedWord[i] =
+          separatedWord[i].charAt(0).toUpperCase() + separatedWord[i].slice(1);
+      }
+
+      return separatedWord.join(' ');
+    }
+  };
+
+  private getInitials = (firstname: string, lastname: string): string => {
+    if (firstname && lastname) {
+      return (
+        firstname.charAt(0).toUpperCase() + lastname.charAt(0).toUpperCase()
+      );
+    }
+  };
 }
